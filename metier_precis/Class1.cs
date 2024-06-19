07276621e95af03504363ed8527c0ddb6ca11a72
CaePolit <caepolit@gmail.com>
2024-06-19 11:25:38 +0200

    Ajoutez des fichiers projet.

---
+++ metier_precis/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metier_precis
+{
+    class Building
+    {
+        public double _hauteur;
+        public int _nombreEtages;
+
+        public Building(int nombreEtages) : this(nombreEtages, nombreEtages)
+        {
+            _nombreEtages = nombreEtages;
+            _hauteur = 3 * nombreEtages;
+        }
+        public Building(int nombreEtages, double hauteur) 
+        {
+            _nombreEtages = nombreEtages;
+            _hauteur = hauteur;
+        }
+
+      
+
+        public double GetSize() {  return _hauteur; }
+        public int GetFloorCount() { return _nombreEtages; }
+
+
+        public double GetFloorMaxSize()
+        {
+            double heigth = GetSize();
+            int nFloors = GetFloorCount();
+            double floorSize = heigth/nFloors;
+            return floorSize;
+        }
+
+        
+
+    }
+}
---
+++ metier_precis/Program.cs
@@ -0,0 +1,17 @@
+﻿using metier_precis;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string entry1 = Console.ReadLine();
+        string entry2 = Console.ReadLine();
+        double hauteur = Convert.ToDouble(entry1);
+        int nombreEtages = Convert.ToInt32(entry2);
+        Building building = new Building(nombreEtages, hauteur);
+        Console.WriteLine(" max floor size: "+ building.GetFloorMaxSize());
+        
+
+    }
+}
